456ad831cf4e8121ee63895aa2340db5acf671f4
Resheph Inayat <rasafinayat@gmail.com>
2024-03-14 12:09:42 +0500

    schema added

--- sanity_first_project/sanity/schema.ts
+++ sanity_first_project/sanity/schema.ts
@@ -1,5 +1,19 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
+const pet = {
+  name: "pet",
+  title: "Pet",
+  type: "document",
+
+  fields: [
+    {
+      name: "pet_name",
+      title: "Pet Name",
+      type: "string"
+    }
+  ] 
+}
+
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [pet],
 }
